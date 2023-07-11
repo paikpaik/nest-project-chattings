8671e7bd30fd4bf1fea820676d121bc9c26ed77a
paikpaik <woduf9311@gmail.com>
2023-07-11 16:47:58 +0900

    Feat: chattings db 설계

---
+++ src/chats/models/chattings.model.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { Socket as SocketModel } from './sockets.model';
+import { Types } from 'mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+const options: SchemaOptions = {
+  //* collection은 디폴트가 있어서 안 적어두 됨. 자동으로 class를 소문자로 바꾸고 복수형으로 만들어줌.
+  collection: 'chattings',
+  timestamps: true,
+};
+
+@Schema(options)
+export class Chatting extends Document {
+  @Prop({
+    type: {
+      _id: { type: Types.ObjectId, requied: true, ref: 'sockets' },
+      id: { type: String },
+      username: { type: String, required: true },
+    },
+  })
+  @IsNotEmpty()
+  user: SocketModel;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  chat: string;
+}
+
+export const ChattingSchema = SchemaFactory.createForClass(Chatting);
---
+++ src/chats/models/sockets.model.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+const options: SchemaOptions = {
+  //* collection은 디폴트가 있어서 안 적어두 됨. 자동으로 class를 소문자로 바꾸고 복수형으로 만들어줌.
+  id: false,
+  collection: 'sockets',
+  timestamps: true,
+};
+
+@Schema(options)
+export class Socket extends Document {
+  @Prop({
+    unique: true,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+}
+
+export const SocketSchema = SchemaFactory.createForClass(Socket);
