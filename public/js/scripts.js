c34a16c4f53e98b762c0632b90582cbb07c80a2e
paikpaik <woduf9311@gmail.com>
2023-07-11 14:35:57 +0900

    Feat: socket.io의 emit&on를 사용하여 서버와 통신

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,6 +8,13 @@ const formElement = getElementById('chat_form');
 
 function helloUser() {
   const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => {
+    console.log(data);
+  });
+  console.log(username);
+  socket.on('hello_user', (data) => {
+    console.log(data);
+  });
 }
 
 function init() {
